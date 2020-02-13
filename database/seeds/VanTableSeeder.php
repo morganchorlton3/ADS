0988f883364481182be0150db6a05c4cfa5b1f90
Morgan Chorlton <contact@morganchorlton.me>
2020-02-13 10:27:33 +0000

    update for server seed

--- database/seeds/VanTableSeeder.php
+++ database/seeds/VanTableSeeder.php
@@ -15,10 +15,10 @@ class VanTableSeeder extends Seeder
         DeliveryVehicle::create(['registration' => 'AB12 ABC', 'mileage' => '12234', 'capacity' => '3500']);
         DeliveryVehicle::create(['registration' => 'AB13 ABC', 'mileage' => '12234', 'capacity' => '3500']);
         DeliveryVehicle::create(['registration' => 'AB14 ABC', 'mileage' => '12234', 'capacity' => '3500']);
-        DeliveryVehicle::create(['registration' => 'AB15 ABC', 'mileage' => '12234', 'capacity' => '3500']);
-        DeliveryVehicle::create(['registration' => 'AB16 ABC', 'mileage' => '12234', 'capacity' => '3500']);
-        DeliveryVehicle::create(['registration' => 'AB17 ABC', 'mileage' => '12234', 'capacity' => '3500']);
-        DeliveryVehicle::create(['registration' => 'AB18 ABC', 'mileage' => '12234', 'capacity' => '3500']);
-        DeliveryVehicle::create(['registration' => 'AB19 ABC', 'mileage' => '12234', 'capacity' => '3500']);
+        //DeliveryVehicle::create(['registration' => 'AB15 ABC', 'mileage' => '12234', 'capacity' => '3500']);
+        //DeliveryVehicle::create(['registration' => 'AB16 ABC', 'mileage' => '12234', 'capacity' => '3500']);
+        //DeliveryVehicle::create(['registration' => 'AB17 ABC', 'mileage' => '12234', 'capacity' => '3500']);
+        //DeliveryVehicle::create(['registration' => 'AB18 ABC', 'mileage' => '12234', 'capacity' => '3500']);
+        //DeliveryVehicle::create(['registration' => 'AB19 ABC', 'mileage' => '12234', 'capacity' => '3500']);
     }
 }
