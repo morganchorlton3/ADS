83362089d713446bc98d02751715ae4e321d020b
morganchorlton3 <morganchorlton3@gmail.com>
2020-03-17 13:40:08 +0000

    general cleanup and styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,26 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-lg-9" style="margin-top:7vh;">
-            <div class="row">
-                @foreach($products as $product)
-                <div class="col-lg-3 col-md-6 mb-4">
-                    <div class="card h-100">
-                        <div class="d-flex justify-content-center">
-                            <a href="#"><img class="text-center" width="150px" height="150px;" src="{{asset('ProductImages/' . $product->barcode . '/1.jpeg')}}" alt=""></a>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-title">
-                                <a href="#">{{ $product->name }}</a>
-                            </p>
-                            <h5>{{ formatPrice($product->price)}}</h5>
-                            <p class="card-text">{{ $product->shortDesc}}</p>
-                            <a href="{{ route('cart.add', $product->id) }}" style="width:100%;" class="btn btn-primary">Add To Cart</a>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-            <div class="row">
-                <div class="col-lg-12 mt-4 mb-4">
-                    <div class="d-flex justify-content-center">
-                        {{ $products->links() }} 
-                    </div>
-                </div>
-            </div>
-        </div>
-        @include('partials.shop.cart')
-        
-    </div>
-</div>
 
+@section('content')
+<div class="row">
+    <div class="col-lg-9">
+        <div class="row pl-2 pr-2">
+        @foreach($products as $product)
+            @include('partials.shop.product')
+        @endforeach
+        </div>
+        <div class="col-lg-12 mt-4 mb-4">
+            <div class="d-flex justify-content-center">
+                {{ $products->links() }} 
+            </div>
+        </div>  
+    </div>
+    <div class="col-lg-3 pr-0 d-none d-lg-block">
+        @include('layouts.partials.cart')
+    </div>
+</div>     
 @endsection
+
 @section('addedJS')
 
 @endsection
