c89b4d7a6fc1290c2c228d15b660be5685a7846a
morganchorlton3 <morganchorlton3@gmail.com>
2020-04-01 21:24:21 +0100

    upgraded to laravel 7

--- config/app.php
+++ config/app.php
@@ -174,8 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        GoogleMaps\ServiceProvider\GoogleMapsServiceProvider::class,
         App\Providers\CartServiceProvider::class,
 
     ],
@@ -228,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GoogleMaps' => GoogleMaps\Facade\GoogleMapsFacade::class,
         'Cart' => App\Facades\CartFacade::class,
 
     ],
